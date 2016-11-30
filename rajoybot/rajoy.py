317f4e745ba14bbeed171ea941f9decb4625257e
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-11-30 15:38:33 +0000

    Scraped some tweets

--- rajoybot/rajoy.py
+++ rajoybot/rajoy.py
@@ -1,3 +1,4 @@
+import csv
 import tweepy
 import config  # File with Twitter's API keys. Hidden from Version Control
 from tweepy import Stream, StreamListener
@@ -23,8 +24,41 @@ def authenticate(config):
     auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
     return tweepy.API(auth)
 
+
+def get_all_tweets(api, screen_name):
+    '''Get all tweets of a user.
+    Taken from https://gist.github.com/yanofsky/5436496
+    Only 3240 most recent tweets'''
+    all_tweets = []
+    new_tweets = api.user_timeline(screen_name=screen_name,
+                                   count=200,
+                                   include_rts=False)
+    all_tweets.extend([tweet for tweet in new_tweets if not tweet.text.startswith('RT ')])
+
+    # save the id of the oldes tweets minus one
+    oldest = all_tweets[-1].id - 1
+
+    # keep grabbing tweets until we can't anymore
+    while len(new_tweets) > 0:
+        new_tweets = api.user_timeline(screen_name=screen_name,
+                                       count=200,
+                                       max_id=oldest,
+                                       include_rts=False)
+        all_tweets.extend([tweet for tweet in new_tweets if not tweet.text.startswith('RT ')])
+
+        oldest = all_tweets[-1].id - 1
+
+    out_tweets = [tweet.text for tweet in all_tweets]
+
+
+    return out_tweets
+
 if __name__ == '__main__':
     api = authenticate(config)
     myStreamListener = MyStreamListener()
     myStream = Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(track=['Rajoy'])
+    out_tweets = get_all_tweets(api, 'marianorajoy')
+    with open('rajoy_tweets.txt', 'w') as f:
+        f.write('\n'.join(str(i) for i in out_tweets))
+
+
